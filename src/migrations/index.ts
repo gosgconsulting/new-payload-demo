9180b5374d183be2ae8eb01ec9b01ae22e8f0b9d
Win Khant Paing <winkhantpaing32@gmail.com>
2025-08-06 10:42:16 +0700

    migration files created

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250806_034127 from './20250806_034127';
+
+export const migrations = [
+  {
+    up: migration_20250806_034127.up,
+    down: migration_20250806_034127.down,
+    name: '20250806_034127'
+  },
+];
