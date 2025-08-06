cf45305ef8754374a22f923e036091d22c681983
gosgconsulting <167956729+gosgconsulting@users.noreply.github.com>
2025-08-06 12:08:09 +0700

    Update entrypoint.sh to use user id 1001

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Set permissions on the media volume
 echo "Setting permissions on the media volume..."
-chown -R appuser:appgroup /app/public/media
+chown -R 1001:1001 /app/public/media
 
 # Execute the original command
 echo "Starting Payload CMS..."
